81bceb025d339cdb88a2feab9e2252e88651424c
alevtina-zabegaeva <95501508+alevtina-zabegaeva@users.noreply.github.com>
2021-12-07 09:09:13 +0100

    Add files via upload

---
+++ 7.1.py
@@ -0,0 +1,21 @@
+def fuel(x):
+    f = 0
+    for crab in inpt:
+        f += abs(crab - x)
+    return f
+
+
+# with open('test7.txt', 'r') as f:
+with open('input7.txt', 'r') as f:
+    inpt = list(map(int, f.read().split(',')))
+
+left, right = min(inpt), max(inpt)
+eps = 2
+while right - left > eps:
+    a = (left * 2 + right) // 3
+    b = (left + right * 2) // 3
+    if fuel(a) < fuel(b):
+        right = b
+    else:
+        left = a
+print(fuel((left + right) // 2))
